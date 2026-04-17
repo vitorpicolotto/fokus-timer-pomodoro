15bb5ad33a485a36711ee8e56c69eb0bb21b51cb
Vitor Picolotto <picolottovitor@gmail.com>
2026-04-17 17:15:03 -0300

    adicionando música e alterando classes dos botões

--- script.js
+++ script.js
@@ -4,25 +4,41 @@ let curtoBt = document.querySelector('.app__card-button--curto');
 let longoBt = document.querySelector('.app__card-button--longo');
 let banner = document.querySelector('.app__image');
 let titulo = document.querySelector('.app__title');
+let botoesAtivos = document.querySelectorAll('.app__card-button');
+let musicaFocoInput = document.querySelector('#alternar-musica');
+let musica = new Audio('/sons/luna-rise-part-one.mp3');
+musica.loop = true
 
-
-
+musicaFocoInput.addEventListener('change', () => {
+    if (musica.paused){
+        musica.play()
+    } else {
+        musica.pause()
+    }
+})
 
 focoBt.addEventListener('click', () => {
-    alteraContexto('foco')
+    alteraContexto('foco');
+    focoBt.classList.add('active');
 });
 
 curtoBt.addEventListener('click', () =>{
-    alteraContexto('descanso-curto')
+    alteraContexto('descanso-curto');
+    curtoBt.classList.add('active');
 });
 
 longoBt.addEventListener('click', () => {
-    alteraContexto('descanso-longo')
+    alteraContexto('descanso-longo');
+    longoBt.classList.add('active')
 });
 
 function alteraContexto(contexto) {
     html.setAttribute('data-contexto', contexto);
     banner.setAttribute('src', `/imagens/${contexto}.png`);
+    botoesAtivos.forEach(function (contexto) {
+        contexto.classList.remove('active')
+    })
+
     switch (contexto) {
         case 'foco':
             titulo.innerHTML = `Otimize sua produtividade,<br>
